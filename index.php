338026bca6d5ed960904fbed74d3fd749a2b0290
poliana11 <85313185+poliana11@users.noreply.github.com>
2021-06-06 11:14:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="C:/Users/WINDOWS 10/Documents/ProjTCC/bootstrap-4.4.1-dist/bootstrap/style.css">
+    <link rel="stylesheet" href="bootstrap-4.4.1-dist/bootstrap/style.css">
     <!--<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>-->
 
     <!-- Optional JavaScript -->
