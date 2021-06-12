41dff350b3e96cfd62aa75247483fa325763a9e0
poliana11 <85313185+poliana11@users.noreply.github.com>
2021-06-12 14:00:14 -0300

    Update conexaoBD.php

--- crudAdm/conexaoBD.php
+++ crudAdm/conexaoBD.php
@@ -3,6 +3,6 @@
 	//$conexao = mysqli_connect("localhost","root","","bdAdm") or die("Erro de conexão");
 	
 	//Conexão remota
-	$conexao = mysqli_connect("sql10.freemysqlhosting.net","sql10417342","IGRC1fZI1H","sql10417342") or die("Erro de conexão");
+	$conexao = mysqli_connect("remotemysql.com","1La9BEFfXZ","NWkaf5C63U","1La9BEFfXZ") or die("Erro de conexão");
 	mysqli_query($conexao,"set names 'utf8'");
 ?>
